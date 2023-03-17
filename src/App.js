be2d1a002fe756db70c53277ff95578cf39ae7a7
Sudhanshu Kumar <97729053+sudhanshusmith@users.noreply.github.com>
2023-03-17 08:29:37 +0530

    Launching Soon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+
+import Comingsoon from './pages/Comingsoon/Comingsoon';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Comingsoon />
     </div>
   );
 }
